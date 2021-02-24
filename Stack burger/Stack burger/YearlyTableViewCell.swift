a1698604a4bdd36255bf96216b0a50c4c4a2c2ef
Gahee Bin <65492044+gahee22@users.noreply.github.com>
2021-02-25 00:08:56 +0900

    Update Yearly View

---
+++ Stack burger/Stack burger/YearlyTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  YearlyTableViewCell.swift
+//  Stack burger
+//
+//  Created by 빈가희 on 2021/02/22.
+//
+
+import UIKit
+
+class YearlyTableViewCell: UITableViewCell {
+
+}
+
---
+++ Stack burger/Stack burger/YearlyViewController.swift
@@ -0,0 +1,19 @@
+//
+//  YearlyViewController.swift
+//  Stack burger
+//
+//  Created by 빈가희 on 2021/02/22.
+//
+
+import UIKit
+
+class YearlyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
